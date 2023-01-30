207195a96de7457732e05f36134a7cb9f890d1c9
facundoalves1 <facundoalves1@gmail.com>
2023-01-30 05:30:25 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//this is a commet to check the github connection
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+app.use(express.json());
+app.use(bodyParser.json());
+dotenv.config();
+
+app.get('/home',(req,res)=>{
+
+    res.send('Home page');
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server listening on port ${PORT}`);
+});
